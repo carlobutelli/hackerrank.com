97bb04db03d4da1b785210998ddde712fc3a13b8
carlitos26 <carlito@Carlos-MacBook-Pro.local>
2016-04-29 19:01:22 +0200

    insertion sort

---
+++ insertion_sort/insertion_sort.c
@@ -0,0 +1,43 @@
+#include <cstdio>
+#include <cstring>
+#include <string>
+#include <cmath>
+#include <cstdlib>
+#include <cassert>
+#include <iostream>
+using namespace std;
+void insertionSort(int ar_size, int ar[]) { 
+     int i, j ,tmp, p;
+     for (i = 1; i < ar_size; i++) 
+     {
+         j = i;
+         while (j > 0 && ar[j - 1] > ar[j]) 
+         {
+             tmp = ar[j];
+             ar[j] = ar[j - 1];
+             ar[j - 1] = tmp;
+             j--;
+         }//end while loop
+
+         for (p=0; p<ar_size; p++)
+            for (p=0; p<ar_size; p++)
+                cout << ar[p] << " ";
+            cout << endl;
+     }//end for loop
+}//end insertion_sort.
+
+int main(void) {
+   
+    int _ar_size;
+    cin >> _ar_size;
+    //scanf("%d", &_ar_size);
+    int _ar[_ar_size], _ar_i;
+    for(_ar_i = 0; _ar_i < _ar_size; _ar_i++) { 
+        cin >> _ar[_ar_i];
+        //scanf("%d", &_ar[_ar_i]); 
+    }
+
+   insertionSort(_ar_size, _ar);
+   
+   return 0;
+}
