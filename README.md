5d99fef1f0523ba3a1f4463ab6b54abf335b12f6
carlobutelli <dev.butelli@gmail.com>
2020-02-04 15:39:25 +0100

    Updated time in words code

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
-# hackerrank
+# My hackerrank.com
 
-My personal solutions to hackerrank coding challanges
+My personal solutions to hackerrank coding challenges
+
+## To run cpp solutions use 
+```
+g++ -std=c++11 <filename.cpp>
+```
+
+# Required
+- Java 11
+- c++ 4.2.1
+- python 3.7.5
+- Clang 11
--- time_in_words.cpp
+++ time_in_words.cpp
@@ -5,51 +5,64 @@
 #include <algorithm>
 using namespace std;
 
-string time_to_word(int m){
+// use g++ -std=c++11 when compiling
+string time_to_word(int m)
+{
     const vector<string> numbers = {"xxxx", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-        "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
-        "eighteen", "nineteen", "twenty", "twenty one", "twenty two", "twenty three", 
-        "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight", 
-        "twenty nine", "thirty"};
-    if(m < 1 || m > 29){
+                                    "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+                                    "eighteen", "nineteen", "twenty", "twenty one", "twenty two", "twenty three",
+                                    "twenty four", "twenty five", "twenty six", "twenty seven", "twenty eight",
+                                    "twenty nine", "thirty"};
+    if (m < 1 || m > 29)
+    {
         cout << "Bad number: " << m << endl;
-        return "error in tome_to_word!";
-    }    
+        return "error in stringifying the time!";
+    }
     return numbers[m];
 }
 
-int main(){
-    int h;
+int main()
+{
+    int h, m;
+    cout << "Set an hour between 1 and 24: ";
     cin >> h;
-    if(h < 1 || h > 12){
+    if (h < 1 || h > 24) {
         cout << "Hour format wrong!";
         return -1;
     }
-    int m;
+    cout << "Set minutes between 0 and 59: ";
     cin >> m;
+    if (m < 0 || h > 59) {
+        cout << "Minutes format wrong!";
+        return -1;
+    }
     string num;
     string hour = time_to_word(h);
 
-    if(m == 0)
+    if (m == 0)
         cout << hour << " o' clock";
-    else if(m > 0 && m < 15 || m > 15 && m < 30){
+    else if ((m > 0 && m < 15) || (m > 15 && m < 30))
+    {
         num = time_to_word(m);
-        cout << num << " minutes past " << hour;   
-    }else if(m == 15)
+        cout << num << " minutes past " << hour;
+    }
+    else if (m == 15)
         cout << "quarter past " << hour;
-    else if(m == 30)
+    else if (m == 30)
         cout << "half past " << hour;
-    else if(m > 30){
-        string next_hour = (h < 12 ? time_to_word(h+1) : time_to_word(1));
+    else if (m > 30)
+    {
+        string next_hour = (h < 12 ? time_to_word(h + 1) : time_to_word(1));
         int temp = 60 - m;
-        if(temp == 15)           
+        if (temp == 15)
             cout << "quarter to " << next_hour;
-        else if(temp == 1)
+        else if (temp == 1)
             cout << "one minute to " << next_hour;
-        else { 
+        else
+        {
             num = time_to_word(temp);
-            cout << num << " minutes to " << next_hour;    
-        } 
+            cout << num << " minutes to " << next_hour;
+        }
     }
     return 0;
 }
